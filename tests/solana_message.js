39d7719f6057cff53b0dca83620ec30e74232123
Yunge Yu <yunge_yu@hotmail.co.nz>
2021-12-26 21:26:06 +1300

    updated

--- tests/solana_message.js
+++ tests/solana_message.js
@@ -1,14 +1,44 @@
-const anchor = require('@project-serum/anchor');
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe('solana_message', () => {
+describe("solana_message", () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaMessage;
+  
+  it("It initializes the account", async () => {
+    const baseAccount = anchor.web3.Keypair.generate();
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+    await program.rpc.initialize("Hello World", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [baseAccount],
+    });
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.SolanaMessage;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Data: ', account.dataList.at(-1));
+    assert.ok(account.dataList.at(-1) === "Hello World");
+    _baseAccount = baseAccount;
+  });
+
+  it("Updates a previously created account", async () => {
+    const baseAccount = _baseAccount;
+
+    await program.rpc.update("Some new data", {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+    console.log('Updated data: ', account.dataList.at(-1))
+    assert.ok(account.data === "Some new data");
+    console.log('all account data:', account)
+    console.log('All data: ', account.dataList);
+    assert.ok(account.dataList.length === 2);
   });
 });
